c0cbc5a2110fa5b9e1d0312fbd6ebe831cdaaaa2
MuliavinIM <vmmuliavin@gmail.com>
2025-12-09 20:56:33 +0500

    Task4

---
+++ Tyuiu.MuliavinIM.Sprint4.Task4.V18.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.MuliavinIM.Sprint4.Task4.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
