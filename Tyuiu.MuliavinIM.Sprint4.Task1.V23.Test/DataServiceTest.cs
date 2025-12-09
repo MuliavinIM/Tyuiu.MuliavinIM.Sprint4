1a1c44f324fc8fc1422ca23b60b8ff36e7cd4500
MuliavinIM <vmmuliavin@gmail.com>
2025-12-09 20:47:32 +0500

    Task1

---
+++ Tyuiu.MuliavinIM.Sprint4.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MuliavinIM.Sprint4.Task1.V23.Lib;
+namespace Tyuiu.MuliavinIM.Sprint4.Task1.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 4, 5, 9, 6, 4, 5 };
+            int res = ds.Calculate(array);
+            int wait = 14;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MuliavinIM.Sprint4.Task1.V23/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.MuliavinIM.Sprint4.Task1.V23.Lib;
+namespace Tyuiu.MuliavinIM.Sprint4.Task1.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (ввод с клавиатуры)                           *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #23                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет сумму четных элементов массива    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int len;
+            Console.Write("Введите количество элементов массива: ");
+            len = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[len];
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write("Введите значение " + i + " элемента массива: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+        }
+    }
+}
