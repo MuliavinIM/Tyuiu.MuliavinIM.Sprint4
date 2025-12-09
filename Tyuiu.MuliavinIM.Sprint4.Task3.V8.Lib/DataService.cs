b91753d3b43dedc91aa93e2aa31a73f69420b4bf
MuliavinIM <vmmuliavin@gmail.com>
2025-12-09 20:53:55 +0500

    Task3

---
+++ Tyuiu.MuliavinIM.Sprint4.Task3.V8.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MuliavinIM.Sprint4.Task3.V8.Lib
+{
+    public class DataService : ISprint4Task3V8
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int k = 1;
+            int min = array[0, 3];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (array[i, 3] < min)
+                        min = array[i, 3];
+                }
+            }
+            return min;
+        }
+    }
+}
