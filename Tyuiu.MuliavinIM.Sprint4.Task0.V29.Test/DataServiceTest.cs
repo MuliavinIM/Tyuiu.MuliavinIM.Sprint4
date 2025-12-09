dddc39e3f1f82f91b62469188c2e4dfe13bb2353
lazer <lazer@PAVLIK>
2025-12-09 20:24:25 +0500

    Add project files.

---
+++ Tyuiu.MuliavinIM.Sprint4.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MuliavinIM.Sprint4.Task0.V29.Lib;
+namespace Tyuiu.MuliavinIM.Sprint4.Task0.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] nums = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+
+            int res = ds.GetSumOddArrEl(nums);
+            int wait = 32;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MuliavinIM.Sprint4.Task0.V29/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.MuliavinIM.Sprint4.Task0.V29.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* Спринт #4                                                                                   *");
+        Console.WriteLine("* Тема: Обработка структурных типов                                                           *");
+        Console.WriteLine("* Задание #0                                                                                  *");
+        Console.WriteLine("* Вариант #29                                                                                 *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                                    *");
+        Console.WriteLine("* Подсчитать сумму нечетных элементов массива                                                 *");
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                            *");
+        Console.WriteLine("***********************************************************************************************");
+
+
+        int[] nums = { 7, 4, 3, 2, 1, 5, 9, 8, 7, 4 };
+
+        Console.WriteLine("Исходный массив: ");
+
+        for (int i = 0; i <= nums.Length - 1; i++)
+        {
+            Console.WriteLine(nums[i] + "\t");
+        }
+
+
+
+
+        Console.WriteLine();
+        Console.WriteLine("***********************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                  *");
+        Console.WriteLine("***********************************************************************************************");
+
+        int res = ds.GetSumOddArrEl(nums);
+
+        Console.WriteLine("Сумма нечётных элементов массива: " + res);
+
+        Console.ReadKey();
+
+    }
+}
