71f39373f4f384d5c1cbf40dee2caaae6c55876b
MuliavinIM <vmmuliavin@gmail.com>
2025-12-09 21:12:44 +0500

    Task5

---
+++ Tyuiu.MuliavinIM.Sprint4.Task5.V21.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MuliavinIM.Sprint4.Task5.V21.Lib;
+
+namespace Tyuiu.MuliavinIM.Sprint4.Task5.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[3, 3] { { 2, -1, 0 },
+                                            { -2, -7, 1 },
+                                            { -8, 2, -4 } };
+            int[,] res = ds.Calculate(matrix);
+            int[,] wait = { { 1, -1, 0 },
+                            { -2, -7, 1 },
+                            { -8, 1, -4 } };
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
