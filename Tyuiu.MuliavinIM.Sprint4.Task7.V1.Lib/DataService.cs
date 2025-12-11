bf43fb388ab38f0f392bbeaef09620fd1736ac49
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-12-12 00:31:55 +0500

    Task7

--- Tyuiu.MuliavinIM.Sprint4.Task7.V1.Lib/DataService.cs
+++ Tyuiu.MuliavinIM.Sprint4.Task7.V1.Lib/DataService.cs
@@ -7,16 +7,20 @@ namespace Tyuiu.MuliavinIM.Sprint4.Task7.V1.Lib
         public int Calculate(int n, int m, string value)
         {
             int[,] matrix = new int[n, m];
+            int index = 0;
+            int countEven = 0;
 
+            // Заполняем матрицу слева направо
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
                 {
-                    matrix[i, j] = int.Parse(value.Substring(i * m + j, 1));
+                    matrix[i, j] = int.Parse(value[index].ToString());
+                    index++;
                 }
             }
 
-            int countEven = 0;
+            // Считаем чётные числа
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
