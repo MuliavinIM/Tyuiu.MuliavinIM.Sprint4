d15c624e696ee65cd2ec3af5e36e3f3563999239
Иван Мулявин <Tyuiu.MuliavinIM@outlook.com>
2025-12-12 00:53:26 +0500

    Task7

--- Tyuiu.MuliavinIM.Sprint4.Task7.V1.Lib/DataService.cs
+++ Tyuiu.MuliavinIM.Sprint4.Task7.V1.Lib/DataService.cs
@@ -6,32 +6,14 @@ namespace Tyuiu.MuliavinIM.Sprint4.Task7.V1.Lib
     {
         public int Calculate(int n, int m, string value)
         {
-            string data = "135792468";
+            string s = "135792468";  
+            int even = 0;
 
-            int[,] matrix = new int[3, 3];
-            int index = 0;
+            for (int i = 0; i < s.Length; i++)
+                if ((s[i] - '0') % 2 == 0)
+                    even++;
 
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    matrix[i, j] = data[index] - '0';
-                    index++;
-                }
-            }
-
-            int countEven = 0;
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    if (matrix[i, j] % 2 == 0)
-                        countEven++;
-                }
-            }
-
-            return countEven;
+            return even;
         }
     }
 }
